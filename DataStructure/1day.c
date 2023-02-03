51cceea13685ca2ec695f7f158c1e3b44d9a54a2
lala-david <wnswns1946@naver.com>
2023-02-03 22:10:50 +0900

    ex1

--- DataStructure/1day.c
+++ DataStructure/1day.c
@@ -1,9 +1,26 @@
 #include<stdio.h>
+#pragma warning(disable:4996)
 
-// pointer address output
+// int * array = array []
+int caculate_sum(int * array) {
+	int sum, i;
+	sum = 0;
+	for (i = 0; i < 10; i++) {
+		sum += array[i];
+	}
+	return sum;
+}
+
 int main(void) {
-	int x = 12;
-	int* ip;
-	ip = &x;
-	printf("0x%p\n", &ip);
+	int sum, i, avg;
+	int num[10];
+	for (i = 0; i < 10; i++) {
+		scanf("%d", &num[i]);
+	}
+	sum = caculate_sum(num);
+	avg= sum / 10;
+	prinf("%d\n", &avg);
+	return 0;
+
+
 }
