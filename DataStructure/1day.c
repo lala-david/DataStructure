f371d3dc098030b31e2acd7e39c26ec3c1436943
lala-david <wnswns1946@naver.com>
2023-02-03 23:03:34 +0900

    completed

--- DataStructure/1day.c
+++ DataStructure/1day.c
@@ -25,18 +25,24 @@ int main(void) {
 	// array size 
 
 	int* array = (int*)malloc(4 * sizeof(int));
+	// int array[4];
 
 	array[0] = 1;
 	array[1] = 2;
-	array[3] = 3;
+	*(array + 2) = 3;
 
 	int* tmp = (int*)malloc(8 * sizeof(int));
 
+	int i;
+	for (i = 0; i < 4; i++) {
+		tmp[i] = array[i];
+	}
+	array = tmp;
 
-
+	array[4] = 4;
+	*(array + 5) = 5;
+	printf("%d \n", array[5]);
 	return 0;
-
-
 }
 
 
