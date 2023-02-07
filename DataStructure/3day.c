b992a44b5f3f1d31bf3e4f4c1bd9f1e7c9b03615
SeongJun <37481441+lala-david@users.noreply.github.com>
2023-02-07 20:20:48 +0900

    Add files via upload

---
+++ DataStructure/3day.c
@@ -0,0 +1,33 @@
+﻿ 
+#include <stdio.h>
+#include <string.h>
+
+#define BUFFER_SIZE 20
+
+int main()
+{
+	char buffer[BUFFER_SIZE];
+
+	while (1) {
+		printf("$ ");   
+		read_line(buffer, BUFFER_SIZE);
+		buffer[strlen(buffer)-1] = '\0';
+		printf("%s:%d \n", buffer, strlen(buffer));
+
+	}
+	return 0;
+}
+
+
+int read_line(char str[], int limit) {
+	int ch, i = 0;
+
+	while ((ch = getchar()) != '\n') {
+		if (i < limit) {
+			str[i++] = ch;
+		}
+		str[i] = '\0';
+		return i;
+	}
+}
+	
